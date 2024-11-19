8d22311c74547d64c5c5c23ba81d1b98f6324a37
JSGehlen <mail@jeremygehlen.com>
2024-11-19 18:43:44 +0200

    pattern update

--- patterns/cta-grid.php
+++ patterns/cta-grid.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: CTA Grid
  * Slug: bruno-the-frog-theme/cta-grid
@@ -9,24 +10,124 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Bruno - CTA Section"},"align":"full","className":"cta-section","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"forest-green","textColor":"white","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull cta-section has-white-color has-forest-green-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"metadata":{"name":"Container"},"align":"full","className":"cta-section__container","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-  <div class="wp-block-group alignfull cta-section__container" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-4"} -->
-    <h2 class="wp-block-heading has-text-align-center has-heading-level-4-font-size">Muut tekevät somea - me teemme somella tuloksia!</h2>
+<!-- wp:group {"metadata":{"name":"Bruno - CTA Grid","categories":["call-to-action"],"patternName":"bruno-the-frog-theme/cta-section"},"align":"full","className":"cta-grid","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"backgroundColor":"off-white","textColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull cta-grid has-black-color has-off-white-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"metadata":{"name":"Container"},"align":"full","className":"cta-grid__title-container","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+  <div class="wp-block-group alignfull cta-grid__title-container" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-4"} -->
+    <h2 class="wp-block-heading has-text-align-center has-heading-level-4-font-size">Miksi sosiaaliseen mediaan panostaminen kannattaa</h2>
     <!-- /wp:heading -->
 
-    <!-- wp:group {"metadata":{"name":"Content"},"className":"cta-section__container__content","style":{"spacing":{"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-    <div class="wp-block-group cta-section__container__content"><!-- wp:paragraph {"align":"center"} -->
-      <p class="has-text-align-center">Bruno the Frog (kavereiden kesken Bruno) on strateginen sometoimisto, joka auttaa sinua saamaan mitattavia tuloksia sosiaalisessa mediassa.</p>
-      <!-- /wp:paragraph -->
+    <!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center">Sosiaalisen median avulla niin pienten kuin suurtenkin yritysten on mahdollista tavoittaa kohderyhmänsä helposti. Sinun ei kuitenkaan tarvitse uskoa vain meidän sanaamme: monet eri tutkimukset ja tilastot todistavat sosiaalisen median tarjoamat mahdollisuudet.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
 
-      <!-- wp:paragraph {"align":"center"} -->
-      <p class="has-text-align-center">Olemme keskittäneet palvelumme erityisesti kolmeen somekanavaan:<br>Facebookiin, Instagramiin ja LinkedIniin.</p>
-      <!-- /wp:paragraph -->
+  <!-- wp:group {"align":"wide","className":"cta-grid__grid-row","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+  <div class="wp-block-group alignwide cta-grid__grid-row"><!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"latte","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-latte-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">4,5 milj.</h2>
+      <!-- /wp:heading -->
 
-      <!-- wp:paragraph {"align":"center"} -->
-      <p class="has-text-align-center">Myös muu markkinointi onnistuu osaavan ja laajan verkostomme kautta.</p>
-      <!-- /wp:paragraph -->
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>Sosiaalisen median käyttäjien määrä Suomessa tammikuussa 2024.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">We Are Social, Meltwater &amp; DataReportal</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"green","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-green-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">86 %</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>86 % sosiaalista mediaa hyödyntävistä yrityksistä kansainvälisesti sanoo somen lisänneen yrityksen brändinäkyvyyden kasvua.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">Statista</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"latte","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-latte-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">62 %</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>Yli puolet suomalaisista käyttää jotakin sosiaalisen median kanavaa päivittäin tai lähes päivittäin.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">We Are Social, Meltwater &amp; DataReportal</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"green","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-green-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">26.1 %</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>Kansainvälisessä tutkimuksessa selvisi, että yli neljäsosa somekäyttäjistä hyödyntää sosiaalista mediaa löytääkseen ostettavia tuotteita.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">We Are Social, Meltwater &amp; DataReportal</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"latte","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-latte-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">78 %</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>Kansainvälisen tutkimuksen mukaan 78 % sosiaalista mediaa hyödyntävistä B2B-markkinoijista koki markkinoinnin somessa tehokkaaksi yrityksen brändille</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">Hubspot &amp; Mention</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"cta-grid__grid-cell","backgroundColor":"green","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group cta-grid__grid-cell has-green-background-color has-background"><!-- wp:heading {"className":"cta-grid__grid-cell\u002d\u002dtitle","style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-2","fontFamily":"sigmar-one"} -->
+      <h2 class="wp-block-heading cta-grid__grid-cell--title has-sigmar-one-font-family has-heading-level-2-font-size" style="line-height:1.2">93,2 %</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"className":"cta-grid__grid-cell\u002d\u002dtext","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group cta-grid__grid-cell--text"><!-- wp:paragraph -->
+        <p>Kansainvälisesti 93,2 % 16-64-vuotiaista käyttää sosiaalista mediaa.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"caption"} -->
+        <p class="has-caption-font-size" style="font-style:normal;font-weight:700">We Are Social, Meltwater &amp; DataReportal</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
