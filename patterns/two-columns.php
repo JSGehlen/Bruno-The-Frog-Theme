1e02fc20d4af2558ae92daf377565a9dffbce9c6
JSGehlen <mail@jeremygehlen.com>
2024-11-20 18:38:02 +0200

    Update two-columns.php

--- patterns/two-columns.php
+++ patterns/two-columns.php
@@ -8,8 +8,7 @@
  * Description: A call-to-action grid with a heading and paragraphs.
  */
 ?>
-
-<!-- wp:group {"tagName":"section","metadata":{"name":"Bruno - Two Columns"},"align":"full","backgroundColor":"forest-green","layout":{"type":"constrained"}} -->
+<!-- wp:group {"tagName":"section","metadata":{"name":"Bruno - Two Columns","patternName":"bruno-the-frog-theme/two-columns"},"align":"full","backgroundColor":"forest-green","layout":{"type":"constrained"}} -->
 <section class="wp-block-group alignfull has-forest-green-background-color has-background"><!-- wp:group {"metadata":{"name":"Container"},"align":"wide","className":"two-columns","backgroundColor":"forest-green","layout":{"type":"flex","flexWrap":"nowrap"}} -->
   <div class="wp-block-group alignwide two-columns has-forest-green-background-color has-background"><!-- wp:group {"className":"flex-one","layout":{"type":"constrained"}} -->
     <div class="wp-block-group flex-one"><!-- wp:image {"sizeSlug":"large","className":"is-style-default"} -->
@@ -52,6 +51,17 @@
 
         <!-- wp:paragraph -->
         <p>Rakastamme dataa! Seuraamme työmme tuloksia ja toimintamme perustuu aina analytiikkaan. Meiltä saat kattavat ja ymmärrettävät raportit somekanavillasi saavutetuista tuloksista. Varmistamme aina, että sinä ymmärrät. Somekielen ei tarvitse olla rakettitiedettä!</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"className":"two-columns__text\u002d\u002dtext-box","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group two-columns__text--text-box"><!-- wp:heading {"level":3,"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"heading-level-5"} -->
+        <h3 class="wp-block-heading has-heading-level-5-font-size" style="line-height:1.2"><strong>Sisällöt yhdeksällä kielellä</strong></h3>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Hello! Hej! Halló! 你好 ! سڵاو¡ <br>Kauttamme saat sisällöt yhdeksällä kielellä: suomi, englanti, ruotsi, saksa, espanja, tanska, islanti, kiina, kurdi (sorani).</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
