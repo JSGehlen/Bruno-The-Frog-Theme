7a710d201d24096704ec5bb1ad5f01d0760ea7b6
JSGehlen <mail@jeremygehlen.com>
2024-11-22 09:27:57 +0200

    add hero

---
+++ patterns/frontpage-hero.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Title: Frontpage Hero
+ * Slug: bruno-the-frog-theme/frontpage-hero
+ * Categories: hero
+ * Block Types: core/group
+ * Keywords: hero, frontpage
+ * Description: The hero section for the frontpage.
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Bruno - Hero"},"align":"full","className":"hero","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignfull hero"><!-- wp:group {"metadata":{"name":"Container"},"className":"hero__container","backgroundColor":"purple","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group hero__container has-purple-background-color has-background"><!-- wp:image {"width":"256px","sizeSlug":"large"} -->
+    <figure class="wp-block-image size-large is-resized"><img src="https://brunothefrog.fi/wp-content/uploads/2024/11/Paras-kaverisi-somessa.png" alt="" style="width:256px" /></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:image {"width":"256px","sizeSlug":"large"} -->
+    <figure class="wp-block-image size-large is-resized"><img src="https://brunothefrog.fi/wp-content/uploads/2024/11/Bruno-the-Frog-logo-valk.png" alt="" style="width:256px" /></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:buttons -->
+    <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"green","textColor":"black","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}}} -->
+      <div class="wp-block-button"><a class="wp-block-button__link has-black-color has-green-background-color has-text-color has-background has-link-color wp-element-button">Palvelumme</a></div>
+      <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
