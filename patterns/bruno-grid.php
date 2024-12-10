41b24e2f643bc2e8572be8060038b062244cb4f3
JSGehlen <mail@jeremygehlen.com>
2024-12-10 20:25:40 +0200

    updates

---
+++ patterns/bruno-grid.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Title: Bruno Grid
+ * Slug: bruno-the-frog-theme/bruno-grid
+ * Block Types: core/group
+ * Keywords: cta, call to action, grid
+ * Description: A call-to-action grid with a heading and paragraphs.
+ */
+?>
+
+<!-- wp:group {"tagName":"section","metadata":{"name":"Bruno - Grid"},"align":"wide","className":"bruno-grid","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignwide bruno-grid"><!-- wp:group {"align":"wide","className":"bruno-grid__row","style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+  <div class="wp-block-group alignwide bruno-grid__row"><!-- wp:group {"className":"bruno-grid__cell","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}}},"backgroundColor":"latte","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group bruno-grid__cell has-latte-background-color has-background" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-5"} -->
+      <h2 class="wp-block-heading has-text-align-center has-heading-level-5-font-size">Sosiaalisen median ulkoistaminen</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|xs","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Anna meidän hoitaa kaikki sosiaalisen median työsi!</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Pidämme huolta, että sinut huomataan somessa ja saat haluamiasi tuloksia</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"bruno-grid__cell","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}}},"backgroundColor":"off-white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group bruno-grid__cell has-off-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-5"} -->
+      <h2 class="wp-block-heading has-text-align-center has-heading-level-5-font-size">Someapu</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|xs","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Kaipaatko henkilökohtaista ja ketterää apua somen käyttöön? </p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Kaipaatko henkilökohtaista ja ketterää apua somen käyttöön? </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"bruno-grid__cell","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}}},"backgroundColor":"latte","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group bruno-grid__cell has-latte-background-color has-background" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-5"} -->
+      <h2 class="wp-block-heading has-text-align-center has-heading-level-5-font-size">Koulutukset</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|xs","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Koulutamme sinut ja tiimisi somen saloihin</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Kaikki koulutukset räätälöidään tarvittaessa toiveidesi mukaan.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"className":"bruno-grid__cell","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}}},"backgroundColor":"off-white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group bruno-grid__cell has-off-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:heading {"textAlign":"center","fontSize":"heading-level-5"} -->
+      <h2 class="wp-block-heading has-text-align-center has-heading-level-5-font-size">Muut palvelut</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|xs","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+      <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Tarvitsetko grafiikkaa, kieliversioita tai valokuvausapua? </p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","fontSize":"paragraph"} -->
+        <p class="has-text-align-center has-paragraph-font-size">Kattavan verkostomme avulla järjestyvät myös muut markkinoinnin ja viestinnän toimet.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</section>
+<!-- /wp:group -->
