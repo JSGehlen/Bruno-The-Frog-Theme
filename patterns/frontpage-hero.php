fbfd6d3fdd955ca7418dd8ef4697743582f407f1
JSGehlen <mail@jeremygehlen.com>
2024-12-16 14:30:26 +0200

    update hero

--- patterns/frontpage-hero.php
+++ patterns/frontpage-hero.php
@@ -12,25 +12,40 @@
 
 <!-- wp:group {"metadata":{"name":"Bruno - Hero","categories":["hero"],"patternName":"bruno-the-frog-theme/frontpage-hero"},"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"latte","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group alignfull has-latte-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0"><!-- wp:group {"className":"hero","layout":{"type":"default"}} -->
-  <div class="wp-block-group hero"><!-- wp:group {"metadata":{"name":"Container"},"className":"hero__container","layout":{"type":"constrained"}} -->
-    <div class="wp-block-group hero__container"><!-- wp:group {"className":"hero__img","layout":{"type":"constrained"}} -->
-      <div class="wp-block-group hero__img"><!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dcollage"} -->
-        <figure class="wp-block-image size-large hero__cta--collage"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/brunothefrog_etusivu_hero.png" alt="" style="object-fit:cover" /></figure>
+  <div class="wp-block-group hero"><!-- wp:group {"className":"hero__social-links","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group hero__social-links"><!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.facebook.com/people/Bruno-the-Frog/61568183092568/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/fb-hero.png" alt="" /></a></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/insta-hero.svg" alt="" /></a></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/linkedin-hero.svg" alt="" /></a></figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"metadata":{"name":"Container"},"className":"hero__container","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group hero__container"><!-- wp:group {"className":"hero__img replay","layout":{"type":"constrained"}} -->
+      <div class="wp-block-group hero__img replay"><!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dcollage fade-in-from-bottom duration-1000"} -->
+        <figure class="wp-block-image size-large hero__cta--collage fade-in-from-bottom duration-1000"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/brunothefrog_etusivu_hero.png" alt="" style="object-fit:cover" /></figure>
         <!-- /wp:image -->
       </div>
       <!-- /wp:group -->
 
-      <!-- wp:group {"className":"hero__cta","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-      <div class="wp-block-group hero__cta"><!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dbestie"} -->
-        <figure class="wp-block-image size-large hero__cta--bestie"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/paraskaverisiblack.png" alt="" style="object-fit:cover" /></figure>
+      <!-- wp:group {"className":"hero__cta replay","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+      <div class="wp-block-group hero__cta replay"><!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dbestie fade-in-from-top duration-1000"} -->
+        <figure class="wp-block-image size-large hero__cta--bestie fade-in-from-top duration-1000"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/paraskaverisiblack.png" alt="" style="object-fit:cover" /></figure>
         <!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dbruno","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-        <figure class="wp-block-image size-large hero__cta--bruno"><img src="https://brunothefrog.fi/wp-content/uploads/2024/11/Bruno-the-Frog-logo-viol.png" alt="" style="object-fit:cover" /></figure>
+        <!-- wp:image {"scale":"cover","sizeSlug":"large","className":"hero__cta\u002d\u002dbruno fade-in-from-top delay-150 duration-1000","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+        <figure class="wp-block-image size-large hero__cta--bruno fade-in-from-top delay-150 duration-1000"><img src="https://brunothefrog.fi/wp-content/uploads/2024/11/Bruno-the-Frog-logo-viol.png" alt="" style="object-fit:cover" /></figure>
         <!-- /wp:image -->
 
-        <!-- wp:buttons -->
-        <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"green","textColor":"black","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}}} -->
+        <!-- wp:buttons {"className":"fade-in-from-top delay-150 duration-1000"} -->
+        <div class="wp-block-buttons fade-in-from-top delay-150 duration-1000"><!-- wp:button {"backgroundColor":"green","textColor":"black","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}}} -->
           <div class="wp-block-button"><a class="wp-block-button__link has-black-color has-green-background-color has-text-color has-background has-link-color wp-element-button" href="#palvelut">Palvelumme</a></div>
           <!-- /wp:button -->
         </div>
