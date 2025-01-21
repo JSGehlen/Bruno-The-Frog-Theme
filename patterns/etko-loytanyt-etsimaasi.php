ec52d998d771337ea58ff438a9e62a6e0266957e
JSGehlen <mail@jeremygehlen.com>
2025-01-21 20:11:57 +0200

    updates

--- patterns/etko-loytanyt-etsimaasi.php
+++ patterns/etko-loytanyt-etsimaasi.php
@@ -9,7 +9,7 @@
  * Description: A section with a heading, paragraphs and a button.
  */
 ?>
-<!-- wp:group {"tagName":"section","metadata":{"name":"Etkö löytänyt etsimääsi?","categories":["text"],"patternName":"bruno-the-frog-theme/section"},"align":"full","className":"section is-style-off-white-green-btn","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"tagName":"section","metadata":{"name":"Etkö löytänyt etsimääsi?","categories":["text"],"patternName":"bruno-the-frog-theme/etko-loytanyt-etsimaasi"},"align":"full","className":"section is-style-off-white-green-btn","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <section class="wp-block-group alignfull section is-style-off-white-green-btn" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"metadata":{"name":"Container"},"align":"full","className":"section__container","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
   <div class="wp-block-group alignfull section__container" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--lg)"><!-- wp:heading {"fontSize":"heading-level-4"} -->
     <h2 class="wp-block-heading has-heading-level-4-font-size">Etkö löytänyt etsimääsi?</h2>
@@ -21,7 +21,7 @@
 
     <!-- wp:buttons -->
     <div class="wp-block-buttons"><!-- wp:button -->
-      <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Ota yhteyttä</a></div>
+      <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="mailto:bruno@brunothefrog.fi">Ota yhteyttä</a></div>
       <!-- /wp:button -->
     </div>
     <!-- /wp:buttons -->
--- patterns/subpage-hero.php
+++ patterns/subpage-hero.php
@@ -9,34 +9,34 @@
  * Description: The hero section for subpages.
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Subpage Hero"},"align":"full","className":"bruno-subpage-hero is-style-latte","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"metadata":{"name":"Subpage Hero","categories":["hero"],"patternName":"bruno-the-frog-theme/subpage-hero"},"align":"full","className":"bruno-subpage-hero is-style-latte","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group alignfull bruno-subpage-hero is-style-latte"><!-- wp:group {"className":"bruno-subpage-hero__container","layout":{"type":"flex","flexWrap":"nowrap"}} -->
   <div class="wp-block-group bruno-subpage-hero__container"><!-- wp:group {"className":"bruno-subpage-hero__container__social-links","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group bruno-subpage-hero__container__social-links"><!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.facebook.com/people/Bruno-the-Frog/61568183092568/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/fb-hero.png" alt="" /></a></figure>
+    <div class="wp-block-group bruno-subpage-hero__container__social-links"><!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.facebook.com/people/Bruno-the-Frog/61568183092568/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunofb.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
       <!-- /wp:image -->
 
-      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/insta-hero.svg" alt="" /></a></figure>
+      <!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunoinsta.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
       <!-- /wp:image -->
 
-      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2024/12/linkedin-hero.svg" alt="" /></a></figure>
+      <!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunolinkedin.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
       <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
 
     <!-- wp:image {"id":342,"sizeSlug":"full","linkDestination":"none","className":"img"} -->
-    <figure class="wp-block-image size-full img"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/Rectangle-2849.png" alt="" class="wp-image-342" /></figure>
+    <figure class="wp-block-image size-full img"><img src="http://bruno-the-frog.local/wp-content/uploads/2025/01/Rectangle-2849.png" alt="" class="wp-image-342" /></figure>
     <!-- /wp:image -->
 
     <!-- wp:group {"className":"bruno-subpage-hero__container__content is-style-default","style":{"spacing":{"blockGap":"var:preset|spacing|2xs"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
     <div class="wp-block-group bruno-subpage-hero__container__content is-style-default"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"heading-level-5"} -->
-      <h2 class="wp-block-heading has-heading-level-5-font-size" style="text-transform:uppercase">Pieni otsikko</h2>
+      <h2 class="wp-block-heading has-heading-level-5-font-size" style="text-transform:uppercase">Palvelumme</h2>
       <!-- /wp:heading -->
 
       <!-- wp:heading {"style":{"typography":{"textTransform":"none"}},"textColor":"purple","fontSize":"heading-level-3"} -->
-      <h2 class="wp-block-heading has-purple-color has-text-color has-heading-level-3-font-size" style="text-transform:none">Pää otsikko</h2>
+      <h2 class="wp-block-heading has-purple-color has-text-color has-heading-level-3-font-size" style="text-transform:none">Sukelletaan someen!</h2>
       <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
