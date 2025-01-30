45760f06d0e53347ffd955959a3c7620a048e96d
JSGehlen <mail@jeremygehlen.com>
2025-01-30 15:17:08 +0200

    update pattern images

--- patterns/image-row.php
+++ patterns/image-row.php
@@ -9,17 +9,17 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Image Row"},"align":"full","className":"bruno-image-row is-style-off-white","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull bruno-image-row is-style-off-white"><!-- wp:image {"sizeSlug":"large"} -->
-  <figure class="wp-block-image size-large"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/palvelummesirukatriina.png" alt="" /></figure>
+<!-- wp:group {"metadata":{"name":"Image Row","patternName":"bruno-the-frog-theme/image-row"},"align":"full","className":"bruno-image-row is-style-off-white","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull bruno-image-row is-style-off-white"><!-- wp:image {"id":595,"sizeSlug":"full","linkDestination":"none"} -->
+  <figure class="wp-block-image size-full"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/siru-katriina-2-brunothefrog.jpg" alt="" /></figure>
   <!-- /wp:image -->
 
-  <!-- wp:image {"sizeSlug":"large"} -->
-  <figure class="wp-block-image size-large"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/palvelummeunsplash.png" alt="" /></figure>
+  <!-- wp:image {"id":593,"sizeSlug":"full","linkDestination":"none"} -->
+  <figure class="wp-block-image size-full"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunothefrog-smileys.jpg" alt="" /></figure>
   <!-- /wp:image -->
 
-  <!-- wp:image {"sizeSlug":"large"} -->
-  <figure class="wp-block-image size-large"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/palvelummeunsplash1.png" alt="" /></figure>
+  <!-- wp:image {"id":596,"sizeSlug":"full","linkDestination":"none"} -->
+  <figure class="wp-block-image size-full"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/siru-katriina-3-brunothefrog.jpg" alt="" /></figure>
   <!-- /wp:image -->
 </div>
 <!-- /wp:group -->
--- patterns/subpage-hero.php
+++ patterns/subpage-hero.php
@@ -11,28 +11,28 @@
 ?>
 <!-- wp:group {"metadata":{"name":"Subpage Hero","categories":["hero"],"patternName":"bruno-the-frog-theme/subpage-hero"},"align":"full","className":"bruno-subpage-hero is-style-latte","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group alignfull bruno-subpage-hero is-style-latte"><!-- wp:group {"className":"bruno-subpage-hero__container","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-  <div class="wp-block-group bruno-subpage-hero__container"><!-- wp:group {"className":"bruno-subpage-hero__container__social-links","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group bruno-subpage-hero__container__social-links"><!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.facebook.com/people/Bruno-the-Frog/61568183092568/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunofb.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
+  <div class="wp-block-group bruno-subpage-hero__container"><!-- wp:group {"metadata":{"name":"Someikonit"},"className":"bruno-subpage-hero__container__social-links","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group bruno-subpage-hero__container__social-links"><!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.facebook.com/people/Bruno-the-Frog/61568183092568/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunofb.svg" alt="" /></a></figure>
       <!-- /wp:image -->
 
-      <!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunoinsta.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
+      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunoinsta.svg" alt="" /></a></figure>
       <!-- /wp:image -->
 
-      <!-- wp:image {"lightbox":{"enabled":false},"aspectRatio":"1","scale":"contain","sizeSlug":"large","linkDestination":"custom"} -->
-      <figure class="wp-block-image size-large"><a href="https://www.instagram.com/bruno_the_social_frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunolinkedin.svg" alt="" style="aspect-ratio:1;object-fit:contain" /></a></figure>
+      <!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"large","linkDestination":"custom"} -->
+      <figure class="wp-block-image size-large"><a href="https://www.linkedin.com/company/bruno-the-frog/" target="_blank" rel=" noreferrer noopener"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/brunolinkedin.svg" alt="" /></a></figure>
       <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:image {"id":342,"sizeSlug":"full","linkDestination":"none","className":"img"} -->
-    <figure class="wp-block-image size-full img"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/Rectangle-2849.png" alt="" class="wp-image-342" /></figure>
+    <!-- wp:image {"id":709,"sizeSlug":"full","linkDestination":"none","className":"img item fade-in-from-top replay"} -->
+    <figure class="wp-block-image size-full img item fade-in-from-top replay"><img src="https://brunothefrog.fi/wp-content/uploads/2025/01/bruno-the-frog-palvelumme.png" alt="" /></figure>
     <!-- /wp:image -->
 
-    <!-- wp:group {"className":"bruno-subpage-hero__container__content is-style-default","style":{"spacing":{"blockGap":"var:preset|spacing|2xs"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-    <div class="wp-block-group bruno-subpage-hero__container__content is-style-default"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"heading-level-5"} -->
-      <h2 class="wp-block-heading has-heading-level-5-font-size" style="text-transform:uppercase">Palvelumme</h2>
+    <!-- wp:group {"className":"bruno-subpage-hero__container__content is-style-default item fade-in-from-bottom replay","style":{"spacing":{"blockGap":"var:preset|spacing|2xs"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+    <div class="wp-block-group bruno-subpage-hero__container__content is-style-default item fade-in-from-bottom replay"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontSize":"1.2rem"}}} -->
+      <h2 class="wp-block-heading" style="font-size:1.2rem;text-transform:uppercase">Palvelumme</h2>
       <!-- /wp:heading -->
 
       <!-- wp:heading {"style":{"typography":{"textTransform":"none"}},"textColor":"purple","fontSize":"heading-level-3"} -->
